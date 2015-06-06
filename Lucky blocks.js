cc79ac8cd32509d6d59fbd4ddc8583dfc6c5d724
andriodjs <cwo41980@goeqa.com>
2015-06-06 16:09:45 -0700

    lucky blocks
    this is for block luncher
    rule:
    dont tell averybody that is  mine

---
+++ Lucky blocks.js
@@ -0,0 +1,13 @@
+// lucky blocks by:andriodjs
+// version 1.0
+if spawn a gold blocks make it 
+Name.blocks.spawn.luckyblocks
+
+// spawn
+spawn a chest
+when spawn a chest
+
+spawn a pig
+when the pigs spawn thats bad lucky
+
+// mods by:andriodjs
